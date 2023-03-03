aff750f317a8749af2eb0c614a803659219fa5f2
Latte <themilkiesyes@gmail.com>
2023-03-03 21:50:07 +0200

    stderr and fixed formatting

--- src/main.c
+++ src/main.c
@@ -1,28 +1,22 @@
 #include "etc.h"
 
-void help() {
-	puts(
-	"piagn: missing file name\n"
-	"piagn <file name>"
-	);
-}
-
 int main(int argc, char* argv[]) {
-	if (argc < 2)
-	{
-		help();
+	if (argc < 2) {
+		fprintf(stderr,
+		"piagn: missing file name\n"
+		"piagn <file name>"
+		);
 		return 1;
 	}
 
-	if (!parse_file(argv[1]))
-	{
-		printf("Error parsing file: %s\n", strerror(errno));
+	if (!parse_file(argv[1])){
+		fprintf(stderr, "Error parsing file: %s\n", strerror(errno));
 		return 1;
 	}
 	
 	struct elem_t_dynarr stack = DYNARR_NEW(elem_t, 16);
 	DYNARR_ASSERT(stack);
 	exec_function(&stack, funcs[0]);
-	
+
 	DYNARR_FREE(stack);
 }
